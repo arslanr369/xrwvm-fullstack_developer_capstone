146e218364eb2583b712a43e6dcc52228c1c7c2f
Arslanr369 <Arslanriaz785@gmail.com>
2024-09-22 14:15:09 +0500

    Your commit message describing the update

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -1,18 +1,20 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+# Uncomment the imports before you add the code 
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # Path for registration
+    # path('register/', views.registration, name='registration'),
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    # Path for login
+    path(route='login', view=views.login_user, name='login'),
 
-    # path for dealer reviews view
+    # Path for dealer reviews view
+    # path('dealer_reviews/', views.get_dealer_reviews, name='dealer_reviews'),
 
-    # path for add a review view
-
+    # Path for add a review view
+    # path('add_review/', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
