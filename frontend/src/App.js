7dcb61e95fac8d2aa9fed6f0e4ddcd8a762439a5
Arslanr369 <arslanriaz785@gmail.com>
2024-09-25 05:01:32 -0400

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import LoginPanel from "./components/Login/Login";
-import RegistrationPanel from "./components/Register/Register"; 
+import PostReview from "./components/Dealers/PostReview"
+import LoginPanel from "./components/Login/Login"
+import RegisterPanel from "./components/Register/Register"
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<RegistrationPanel />} /> 
-      {/* Add more routes here as needed */}
+      <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
-
 export default App;
